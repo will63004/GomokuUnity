053a7984bdcb162a42b9d177e08826cc825e1021
will63004 <will63004@gmail.com>
2020-03-24 09:56:17 +0800

    [Feature] Add GameCore

---
+++ Gomoku/Assets/Scripts/StartUp/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using GameCore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartUp : MonoBehaviour
+{
+    private Gomoku gomoku = new Gomoku(50);
+
+    void Start()
+    {        
+        Debug.Log(gomoku.Size);
+    }
+}
